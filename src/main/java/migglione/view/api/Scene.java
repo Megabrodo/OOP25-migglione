1d790522b82dadd2df0a973f81a6873b6dc78fab
Alessandro Zani <alessandro.zani3@studio.unibo.it>
2026-01-30 06:20:34 +0100

    Added gitaatributes, updated gitignore and first versions of view

---
+++ src/main/java/migglione/view/api/Scene.java
@@ -0,0 +1,6 @@
+package migglione.view.api;
+
+import javax.swing.JPanel;
+
+public class Scene extends JPanel {
+}
---
+++ src/main/java/migglione/view/api/View.java
@@ -0,0 +1,9 @@
+package migglione.view.api;
+
+public interface View {
+    void setScene(Scene scene);
+
+    Scene getScene();
+
+    void refresh();
+}
