dec72195fe20c28757d9970d2b751a8554d7f1bc
Alessandro Zani <alessandro.zani3@studio.unibo.it>
2026-01-30 08:24:24 +0100

    Added support to play soundtracks while on menu

--- src/main/java/migglione/view/impl/SwingViewImpl.java
+++ src/main/java/migglione/view/impl/SwingViewImpl.java
@@ -5,20 +5,20 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.sound.sampled.*;
+import java.io.File;
 
 //import migglione.view.api.Scene;
 import migglione.view.api.SwingView;
 
 public final class SwingViewImpl implements SwingView {
 
-    private static final String START = "Start Game";
-    private static final String GALLERY = "Gallery";
-    private static final String QUIT = "Quit";
     private static final String FRAME_NAME = "Migglione: the game";
 
     private final JFrame frame = new JFrame(FRAME_NAME);
     private final CardLayout cards = new CardLayout();
     private final JPanel firstPanel = new JPanel(cards);
+    private Clip audioClip;
 
     public SwingViewImpl() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,6 +30,23 @@ public final class SwingViewImpl implements SwingView {
         frame.add(firstPanel);
         frame.setVisible(true);
         setScene("MENU");
+
+        playMusic();
+    }
+
+    private void playMusic() {
+        try {
+            String soundtrackPath = "src\\main\\resources\\soundtracks\\Machine-Love-_feat.-Neuro-sama_-Neuro-sama-Community-Collab.wav";
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(soundtrackPath));
+
+            audioClip = AudioSystem.getClip();
+            audioClip.open(audioStream);
+
+            audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+            audioClip.start();
+        } catch (Exception e) {
+            System.err.println("Error in loading music:" + e.getMessage());
+        }
     }
 
     @Override
