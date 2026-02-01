b9ba4a5a0f98ed2eb43f8b404b2654516966b6f6
Alessandro Zani <alessandro.zani3@studio.unibo.it>
2026-02-01 09:01:22 +0100

    Credits is now build friendly

--- src/main/java/migglione/view/impl/Credits.java
+++ src/main/java/migglione/view/impl/Credits.java
@@ -7,12 +7,22 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
+/**
+ * Class Credits scene of the application,
+ * it simply presents the creators and gives
+ * credits to whoever was involved in the project.
+ */
 public final class Credits extends JPanel {
-    
+
     private static final long serialVersionUID = 9879879879L;
     private static final String BACKGROUND_IMAGE_PATH = "/images/utilities/credits.png";
     private final transient Image creditsImage;
 
+    /**
+     * Constructor of Credits.
+     * 
+     * @param view is used to return back to the Menu
+     */
     public Credits(final SwingViewImpl view) {
         this.setLayout(new BorderLayout());
 
