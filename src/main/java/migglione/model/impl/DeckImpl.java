3d96e4e54a9e57028af098a67a7f9f0ab0de4a7a
Alessandro Zani <alessandro.zani3@studio.unibo.it>
2026-02-02 05:26:56 +0100

    Added deck implementation

--- src/main/java/migglione/model/impl/DeckImpl.java
+++ src/main/java/migglione/model/impl/DeckImpl.java
@@ -2,6 +2,7 @@ package migglione.model.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import migglione.model.api.Cards;
 import migglione.model.api.Deck;
@@ -10,26 +11,34 @@ public class DeckImpl implements Deck {
 
     private final Cards cards = new Cards();
     private final List<Integer> deck = new ArrayList<>();
+    private final Random random;
 
     public DeckImpl() {
         createDeck();
+        random = new Random();
     }
 
     private void createDeck() {
-        for (var key : cards.getCards().keySet()) {
-            deck.add(key);
-        }
+        cards.getCards().keySet().stream().forEach(deck::add);
     }
 
     @Override
     public void shuffle() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'shuffle'");
+        List<Integer> temp = new ArrayList<>();
+        int deckSize = deck.size();
+        int n = random.nextInt(deckSize);
+
+        for (int i = deck.size(); i > 0; i--) {
+            temp.addLast(deck.get(n));
+            deck.remove(n);
+            n = random.nextInt(deckSize);
+        }
+
+        deck.addAll(temp);
     }
 
     @Override
     public List<Integer> getCards() {
         return deck;
     }
-    
 }
