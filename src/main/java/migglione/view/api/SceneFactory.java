a086e607bb4c38de41cc536b0692fa6fc92de782
Alessandro Zani <alessandro.zani3@studio.unibo.it>
2026-02-02 07:56:01 +0100

    SceneFactoryImpl build friendly

--- src/main/java/migglione/view/api/SceneFactory.java
+++ src/main/java/migglione/view/api/SceneFactory.java
@@ -12,7 +12,7 @@ public interface SceneFactory {
      * Method used to create the scenes of CardLayout.
      * 
      * @param view is the parent View from which CardLayout is initialized
-     * @param n will be the scene's name
+     * @param n will be parameter to decide what scene it to be created
      * @return a JPanel rapresenting the new Scene
      */
     JPanel createScene(SwingViewImpl view, String n);
--- src/main/java/migglione/view/impl/SceneFactoryImpl.java
+++ src/main/java/migglione/view/impl/SceneFactoryImpl.java
@@ -4,15 +4,23 @@ import javax.swing.JPanel;
 
 import migglione.view.api.SceneFactory;
 
-public class SceneFactoryImpl implements SceneFactory {
+/**
+ * Implementation of SceneFactory.
+ * 
+ * <p>
+ * It lets the creation of the different scenes
+ * up to a factory, in order to incapsulate the names
+ * of the scenes and to allow extentions by simply adding
+ * new values cleanly.
+ */
+public final class SceneFactoryImpl implements SceneFactory {
 
     @Override
     public JPanel createScene(final SwingViewImpl view, final String n) {
-        return switch(n) {
+        return switch (n) {
             case "MENU" -> new Menu(view);
             case "CREDITS" -> new Credits(view);
             default -> throw new IllegalArgumentException("No such scene exist");
         };
     }
-    
 }
