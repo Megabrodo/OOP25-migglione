054906b9ddb3a701d9765ba52e62335a95de0f35
Alessandro Zani <alessandro.zani3@studio.unibo.it>
2026-02-02 07:26:33 +0100

    Scenefactory build friendly

--- src/main/java/migglione/view/api/SceneFactory.java
+++ src/main/java/migglione/view/api/SceneFactory.java
@@ -8,6 +8,12 @@ import migglione.view.impl.SwingViewImpl;
  * Factory needed to create scenes for CardLayout more easily.
  */
 public interface SceneFactory {
-    
+    /**
+     * Method used to create the scenes of CardLayout.
+     * 
+     * @param view is the parent View from which CardLayout is initialized
+     * @param n will be the scene's name
+     * @return a JPanel rapresenting the new Scene
+     */
     JPanel createScene(SwingViewImpl view, String n);
 }
