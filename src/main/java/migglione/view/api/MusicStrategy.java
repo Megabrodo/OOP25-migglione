35a6454a1490497c9171fe743069610c36b0c15c
Alessandro Zani <alessandro.zani3@studio.unibo.it>
2026-02-02 07:22:12 +0100

    MusicStrategy build friendly

--- src/main/java/migglione/view/api/MusicStrategy.java
+++ src/main/java/migglione/view/api/MusicStrategy.java
@@ -1,5 +1,25 @@
 package migglione.view.api;
 
+/**
+ * Functional interface to apply the Strategy method.
+ * 
+ * <p>
+ * This interface makes so every scene can return it's
+ * implementation of the MusicPlayer, with the music
+ * chosen for that scene.
+ * 
+ * <p>
+ * Its true use however is seen in the implementation
+ * of SwingView, since it is responsible for playing
+ * the music of the new scene and eventually stopping the
+ * previous one (check SetScene)
+ */
 public interface MusicStrategy {
+    /**
+     * Method used to return the scene's MusicPlayer.
+     * 
+     * @return the MusicPlayer chosen among its 
+     *         implementations for the scene
+     */
     MusicPlayer getMusic();
 }
