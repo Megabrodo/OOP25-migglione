c869db9c90a1753a3914145d2f81340b1ed91467
Alessandro Zani <alessandro.zani3@studio.unibo.it>
2026-02-02 07:15:32 +0100

    MusicPlayer build friendly

--- src/main/java/migglione/view/api/MusicPlayer.java
+++ src/main/java/migglione/view/api/MusicPlayer.java
@@ -1,7 +1,21 @@
 package migglione.view.api;
 
+/**
+ * Interface used to describes the main methods for playing music.
+ * 
+ * <p>
+ * This way, different types of music can be played and also
+ * the way it behaves can be changed in the different scenes,
+ * upholding to the Strategy method.
+ */
 public interface MusicPlayer {
+    /**
+     * Functional method to start the music.
+     */
     void playMusic();
 
+    /**
+     * Functional method to stop the music.
+     */
     void stopMusic();
 }
