cc9ef3de241114ca1514e9aa03275a3fa6a7f52f
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2026-02-03 12:20:09 +0100

    Modified Match model to be more compile-friendly and Checkstyle friendly.

--- src/main/java/migglione/model/impl/Match.java
+++ src/main/java/migglione/model/impl/Match.java
@@ -6,23 +6,31 @@ import java.util.List;
 import migglione.model.api.Card;
 import migglione.model.api.Player;
 
+/**
+ * Class that covers a match of the game.
+ * Takes care of turn order, card submission, comparisons and score updates.
+ * 
+ */
 public class Match {
 
     private final List<Player> players = new ArrayList<>();
     private final List<List<Card>> winnings = new ArrayList<>();
     private final List<Integer> scores = new ArrayList<>();
-    private int turnStart = 0;
-    
+    private int turnStart;
+
     /**
      * Constructor of the class.
      * Creates a match involving an amount of players,
      * and manages all aspects of a game.
+     * 
+     * @param starter the player that starts the first turn
+     * @param second the player that will go second in the first turn
      */
     public Match(final Player starter, final Player second) {
         // setting up players' scores and winnings
         players.add(starter);
         players.add(second);
-        for (int i = 0; i < players.size(); i++) {
+        for (Player p : players) {
             winnings.add(new ArrayList<>());
             scores.add(0);
         }
@@ -40,30 +48,35 @@ public class Match {
      * </ol>
      */
     public void playTurn() {
-        List<Card> cardsPlayed = new ArrayList<>();
-        final int attrChoice = players.get(turnStart).chooseAttr(); //function not implemented yet
-        final int comparison = compareCards(
-            players.getFirst().playCard(attrChoice), 
-            players.getLast().playCard(attrChoice), 
+        final List<Card> cardsPlayed = new ArrayList<>();
+        final int attrChoice = turnStart; //rudimental sub, should be like: players.get(turnStart).chooseAttr();
+        final int comparison = this.compareCards(cardsPlayed.getFirst(), cardsPlayed.get(1), attrChoice);
+        /* comparison should be like: compareCards(
+            players.getFirst().playCard(attrChoice),
+            players.getLast().playCard(attrChoice),
             attrChoice
-        );
+        );*/
         final int winner = comparison < 0 ? 0 : 1;
         scores.set(winner, scores.get(winner) + Math.abs(comparison));
+        turnStart = (turnStart + 1) % players.size();
     }
+
     /**
      * Compares two given cards on a specific attribute.
+     * 
      * @param a the first card
      * @param b the second card
      * @param stat the given attribute to compare the cards on
      * @return the difference between the two cards' values on the given attribute.
      */
-    private int compareCards(Card a, Card b, int stat) {
+    private int compareCards(final Card a, final Card b, final int stat) {
         switch (stat) {
             case 0: return a.getAttk() - b.getAttk();
             case 1: return a.getDeff() - b.getDeff();
             case 2: return a.getAbiity() - b.getAbiity();
             case 3: return a.getIntelligenza() - b.getIntelligenza();
             case 4: return a.getFurtivita() - b.getFurtivita();
+            default: throw new IllegalArgumentException("Illegal attribute chosen.");
         }
     }
 
