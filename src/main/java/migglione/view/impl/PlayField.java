43c7cacabc07da7c8f7176630ce9f2479f20b587
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2026-02-03 17:13:58 +0100

    Modified PlayField for Checkstyle compatibility.

--- src/main/java/migglione/view/impl/PlayField.java
+++ src/main/java/migglione/view/impl/PlayField.java
@@ -1,6 +1,5 @@
 package migglione.view.impl;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridBagLayout;
 
@@ -9,12 +8,15 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.SoftBevelBorder;
+
 /** 
  * Class for managing the playing field's view, 
  * with both player's hands, decks and scores,
  * along with a settings section.
  * */
 public class PlayField extends JPanel {
+
+    private static final long serialVersionUID = 5456545654L;
 
     /**
      * Constructor of this class. 
@@ -23,23 +25,23 @@ public class PlayField extends JPanel {
      */
     public PlayField() {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        JPanel pCards = new JPanel();
-        JPanel oCards = new JPanel();
-        JPanel mainField = new JPanel();
-        JPanel scoreCol = new JPanel();
-        JButton pScore = new JButton("0");
-        JButton oScore = new JButton("0");
+        final JPanel pCards = new JPanel();
+        final JPanel oCards = new JPanel();
+        final JPanel mainField = new JPanel();
+        final JPanel scoreCol = new JPanel();
+        final JButton pScore = new JButton("0");
+        final JButton oScore = new JButton("0");
         this.add(oCards);
         this.add(mainField);
         this.add(pCards);
 
-        mainField.setBackground(new Color((125 * 2 ^ 16 + 80 * 2 ^ 8)));
+        mainField.setBackground(new Color(125 * 2 ^ 16 + 80 * 2 ^ 8));
         mainField.setBorder(new SoftBevelBorder(1, Color.BLACK, Color.DARK_GRAY));
         pScore.setBackground(Color.BLUE);
         oScore.setBackground(Color.RED);
         pScore.setEnabled(false);
         oScore.setEnabled(false);
-        
+
         mainField.setLayout(new GridBagLayout());
         mainField.add(scoreCol);
         scoreCol.setLayout(new BoxLayout(scoreCol, BoxLayout.Y_AXIS));
