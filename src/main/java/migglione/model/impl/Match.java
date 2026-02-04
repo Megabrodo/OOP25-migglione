7a3b264fcb2e5cb66e3ec52f26752b732f32dbd7
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2026-02-04 16:23:40 +0100

    Implemented minor methods to obtain information about the match.

--- src/main/java/migglione/model/impl/Match.java
+++ src/main/java/migglione/model/impl/Match.java
@@ -12,6 +12,7 @@ import migglione.model.api.Player;
  */
 public class Match {
 
+    private static final int DECKS_SIZE = 24;
     private final Map<Player, Integer> scores = new HashMap<>();
     private int turn = 1;
 
@@ -24,7 +25,7 @@ public class Match {
      * @param second the player that will go second in the first turn
      */
     public Match(final Player starter, final Player second) {
-        // setting up players' scores and winnings
+        // setting up players' scores
         scores.put(starter, 0);
         scores.put(second, 0);
         //other setup needed
@@ -45,11 +46,35 @@ public class Match {
         int compSign = 1;
         int comparison = 0;
         for ( Player p : scores.keySet()) {
-            comparison += p.playCard(attrChoice, p.getHand().getFirst()) * compSign;
+            comparison += p.playCard(attrChoice, p.getHand().getFirst()) * compSign; //card currently chosen automatically
             compSign *= -1;
         }
         final Player winner = scores.keySet().stream().toList().get(comparison <= 0 ? 0 : 1);
         scores.replace(winner, scores.get(winner) + Math.abs(comparison));
         turn++;
+    }
+
+    /**
+     * Method to get the turn the match is on.
+     * 
+     * @return the current turn.
+     */
+    public int getTurn() {
+        return this.turn;
+    }
+
+    /** 
+     * Checks if the match has ended.
+    */
+    public boolean matchEnded() {
+        return this.turn == DECKS_SIZE;
+    }
+
+    public int getScore(Player player) {
+        if(scores.keySet().contains(player)) {
+            return scores.get(player);
+        } else {
+            throw new IllegalArgumentException("Requested score of a player not in this match.");
+        }
     }
 }
