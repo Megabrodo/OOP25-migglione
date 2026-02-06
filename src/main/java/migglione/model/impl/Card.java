2705cd08f4c9725aadd760b01550a93fb29fe179
anton abramov <anton.abramov@studio.unibo.it>
2026-02-06 23:00:36 +0100

    modifie PointsPile and generic checkstyle

--- src/main/java/migglione/model/impl/Card.java
+++ src/main/java/migglione/model/impl/Card.java
@@ -8,7 +8,7 @@ public class Card {
     private final int intelligence;
     private final int stealth;
 
-    public Card (final String name, final int attk, final int deff,
+    public Card(final String name, final int attk, final int deff,
         final int strength, final int intelligence, final int stealth) {
         this.name = name;
         this.attk = attk;
@@ -50,5 +50,5 @@ public class Card {
         + "strength: " + this.strength
         + "intelligence: " + this.intelligence
         + "stealth: " + this.stealth;
-    }  
+    }
 }
--- src/main/java/migglione/model/impl/PointsPile.java
+++ src/main/java/migglione/model/impl/PointsPile.java
@@ -1,40 +1,32 @@
 package migglione.model.impl;
 
-import migglione.model.impl.Cards;
 import java.util.ArrayList;
 import java.util.List;
-import migglione.model.api.CardDraw;
-import migglione.model.api.Deck;
 
 public class PointsPile {
 
-//List<Card> pile = new ArrayList<>();
 private List<Card> pile;
     
-    public PointsPile(){
-        //this.pile = new ArrayList<>();
+    public PointsPile() {
         this.pile = new ArrayList<>();
     }
 
-    public void discard (Card card){
+    public void discard(Card card) {
         pile.add(card);
     }
 
-    public boolean ispileEmpty(){
+    public boolean isPileEmpty() {
         return this.pile.isEmpty();
     }
 
-    public Card scry(){
-        if(ispileEmpty()){
+    public List<Card> getPile() {
+        return this.pile;
+    }
+
+    public Card scry() {
+        if(isPileEmpty()) {
             return null;
         }
         return pile.get(pile.size() -1);
     }
-
-    /*
-    public void replace(ArrayList<Cards> cards){
-        this.pile = pile.toArray(new Cards cards.size());
-        this.
-    }
-    */
 }
