6f3831d55a255f68156886b9f316fc8213d51360
anton abramov <anton.abramov@studio.unibo.it>
2026-02-07 23:38:23 +0100

    Cards javadoc comments

--- src/main/java/migglione/model/impl/Cards.java
+++ src/main/java/migglione/model/impl/Cards.java
@@ -5,13 +5,27 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Class Cards create 24 cards and method to get it.
+ * 
+ * @see CardId where all 24 cards was defined 
+ *             whit their atribute and name
+ */
 public class Cards {
     private final Map<Integer, Card> card;
 
+    /**
+     * Constructor of the cards.
+     */
     public Cards() {
         card = CardId.buildCardsMap();
     }
 
+    /**
+     * Getter for define cards.
+     * 
+     * @return cards
+     */
     public Map<Integer, Card> getCards() {
         return Collections.unmodifiableMap(card);
     }
