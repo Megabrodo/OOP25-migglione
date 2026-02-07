57e7eef4a67e60278df27e399f5249f766d535e9
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2026-02-07 11:16:06 +0100

    Implemented abstract class into playing field's GUI.

--- src/main/java/migglione/view/impl/StartGame.java
+++ src/main/java/migglione/view/impl/StartGame.java
@@ -2,11 +2,13 @@ package migglione.view.impl;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Image;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.SoftBevelBorder;
@@ -14,21 +16,23 @@ import javax.swing.border.SoftBevelBorder;
 import migglione.view.api.music.MusicPlayer;
 import migglione.view.api.music.MusicProvider;
 import migglione.view.impl.musicimpl.LoopingMusicPlayerImpl;
+import migglione.view.impl.scenesimpl.AbstractGamePanel;
 
 /** 
  * Class for managing the playing field's view, 
  * with both player's hands, decks and scores,
  * along with a settings section.
  * */
-public final class StartGame extends JPanel implements MusicProvider {
+public final class StartGame extends AbstractGamePanel implements MusicProvider {
 
-    private static final long serialVersionUID = 5456545654L;
     private static final int RED_HUE = 173;
     private static final int GREEN_HUE = 103;
     private static final int BLUE_HUE = 44;
     private static final Color TABLE_COLOR = new Color(RED_HUE, GREEN_HUE, BLUE_HUE);
     private static final String TRACK_PATH = "/soundtracks/ENA Dream BBQ.wav";
     private static final String CARDS_IMAGE_PATH = "/images/cards/";
+    private static final String BACKGROUND_IMAGE_PATH = "/images/utilities/Table.jpeg";
+    private final transient Image playField;
 
     /**
      * Constructor of this class. 
@@ -36,7 +40,9 @@ public final class StartGame extends JPanel implements MusicProvider {
      * opponent's hand, menu and scores.
      */
     public StartGame() {
-        this.setLayout(new BorderLayout()); //prob is "this inside boxlayout"
+
+        this.playField = new ImageIcon(getClass().getResource(BACKGROUND_IMAGE_PATH)).getImage();
+        this.setLayout(new BorderLayout());
 
         final JPanel pCards = new JPanel();
         final JPanel oCards = new JPanel();
@@ -49,7 +55,6 @@ public final class StartGame extends JPanel implements MusicProvider {
         this.add(mainField, BorderLayout.CENTER);
         this.add(pCards, BorderLayout.SOUTH);
 
-        mainField.setBackground(TABLE_COLOR);
         mainField.setBorder(new SoftBevelBorder(1, Color.BLACK, Color.DARK_GRAY));
         pScore.setBackground(Color.GREEN);
         oScore.setBackground(Color.RED);
@@ -76,5 +81,10 @@ public final class StartGame extends JPanel implements MusicProvider {
     @Override
     public MusicPlayer getMusic() {
         return new LoopingMusicPlayerImpl(TRACK_PATH);
+    }
+
+    @Override
+    protected Image getImage() {
+        return playField;
     }
 }
