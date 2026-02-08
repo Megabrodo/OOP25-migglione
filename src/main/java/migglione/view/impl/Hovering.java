ceae7d2ec215cbeec92960b9cf25db3bfe65a119
Lorenzo Zanoni <lorenzo.zanoni3@studio.unibo.it>
2026-02-08 18:42:52 +0100

    changed hovering to better interface

--- src/main/java/migglione/view/impl/Hovering.java
+++ src/main/java/migglione/view/impl/Hovering.java
@@ -1,7 +1,7 @@
 package migglione.view.impl;
 
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseListener;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -14,7 +14,7 @@ import migglione.model.impl.Card;
  * really only uses the mouseMoved() method, the other is empty 
  * because it has no real fucntion in the game.
  */
-public final class Hovering implements MouseMotionListener {
+public final class Hovering implements MouseListener {
     private final HoveringCard hoveringCard;
     private final JPanel gamePanel;
     private static final String CARDS_IMAGE_PATH = "/images/cards/";
@@ -25,18 +25,34 @@ public final class Hovering implements MouseMotionListener {
     }
 
     @Override
-    public void mouseDragged(MouseEvent e) {
-        //empty for now, might find a function for it
+    public void mouseClicked(MouseEvent e) {
+        //io farei qui la roba thomas, secondo me ti aiuta nel field e fai tutto in uno
     }
 
     @Override
-    public void mouseMoved(MouseEvent e) {
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //da implementare
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
         final Image cardImg = new ImageIcon(getClass().getResource(hoveringCard.getImage())).getImage();
-        //non so se quando va via il cursore va via anche l'immagine, se no bisogna implementare un metodo per farla sparire
         gamePanel.getGraphics().drawImage(cardImg, e.getX(), e.getY(), 100, 150, gamePanel);
         //o faccio immagini per le stat(banale word) oppure cerco di fare un panel in cui si scrive
     }
-    
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+    }
+
     /**
      * Nested class used to create a card with the same stats as the one in the hand but with an image path.
      * Like a card but with an image too, used to create the hovering effect on the buttons.
