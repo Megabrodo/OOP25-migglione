7cd0f0208839ec3e9e2d5e8ca4dd3c26cf670c0b
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2026-02-10 00:50:47 +0100

    Added retriever of playTurn result in Game.

--- src/main/java/migglione/model/impl/Game.java
+++ src/main/java/migglione/model/impl/Game.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
  * Takes care of managing when the turns happen and more
  */
 public class Game extends Match {
-
     /**
      * Constructor of the class.
      * Due to being a "more functional" version of Match,
@@ -21,8 +20,21 @@ public class Game extends Match {
         super(new User(new ArrayList<>()), new Mosquito(new ArrayList<>(), false), new StandardCardDrawImpl(new DeckImpl()));
     }
 
+    /**
+     * Upon user's choice, plays the rest of the turn. 
+     * The chosen card is "played", but only virtually as it is redrawn
+     * in order to set the played card for easier access during calculcations.
+     * 
+     * @param attr
+     * @param played
+     */
     public void userChosen(final String attr, final Card played) {
         this.getPlayers().getFirst().playCard(attr, played);
-        playTurn();
+        this.getPlayers().getFirst().drawCard(played);
+        final boolean end = playTurn();
+        if (end) {
+            //post-game stuff
+        }
+
     }
 }
