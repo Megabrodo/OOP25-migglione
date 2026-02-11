2e075444bd97d11dbfdb9ab3f401197c181f3033
anton abramov <anton.abramov@studio.unibo.it>
2026-02-11 12:56:17 +0100

    Scores

--- src/main/java/migglione/view/impl/scenesimpl/Scores.java
+++ src/main/java/migglione/view/impl/scenesimpl/Scores.java
@@ -1,5 +1,33 @@
 package migglione.view.impl.scenesimpl;
 
-public class Scores {
+import java.awt.Image;
 
+import javax.swing.JFrame;
+import javax.swing.JTable;
+
+import migglione.view.api.music.MusicPlayer;
+import migglione.view.api.music.MusicProvider;
+import migglione.view.impl.SwingViewImpl;
+import migglione.view.impl.musicimpl.LoopingMusicPlayerImpl;
+
+public final class Scores extends AbstractGamePanel implements MusicProvider {
+private static final String TRACK_PATH = "/soundtracks/ENA Dream BBQ.wav";
+private static final String BACKGROUND_IMAGE_PATH;
+
+    Scores(final SwingViewImpl view) {
+
+        
+
+    }
+
+    @Override
+    public MusicPlayer getMusic() {
+        return new LoopingMusicPlayerImpl(TRACK_PATH);
+    }
+
+    @Override
+    protected Image getImage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getImage'");
+    }
 }
