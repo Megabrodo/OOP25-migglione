a187ef95f39fb2afcf9b0d05cd580fb1e9c6aee1
anton abramov <anton.abramov@studio.unibo.it>
2026-02-12 18:06:33 +0100

    Scores checkstle

--- src/main/java/migglione/view/impl/scenesimpl/Scores.java
+++ src/main/java/migglione/view/impl/scenesimpl/Scores.java
@@ -9,11 +9,9 @@ import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextArea;
 
 import migglione.view.api.music.MusicPlayer;
@@ -29,30 +27,17 @@ private static final String BACKGROUND_IMAGE_PATH = "/images/utilities/title.png
 private final transient Image scorImage;
 private static final String BACK = "Back";
 private static final String FILE_TXT_PATH = "/resources/file/ScoreTable.txt";
-private JTextArea score;
+private final JTextArea score;
 
-    public Scores(final SwingViewImpl view) { //final SwingViewImpl view
+    public Scores(final SwingViewImpl view) {
+
         /*
-        this.setLayout(new BorderLayout()); //1
-        scorImage = new ImageIcon(getClass().getResource(BACKGROUND_IMAGE_PATH)).getImage();
-
-        final JPanel pSouth = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        final JButton back = new GenericButton(BACK, b -> view.setScene(Scenes.MENU.getScene()));
-
-        pSouth.setOpaque(false);
-        pSouth.add(back);
-        this.add(pSouth, BorderLayout.SOUTH);  
-
-        //this.scorImage = new Image();
         scorImage = new ImageIcon(getClass().getResource(BACKGROUND_IMAGE_PATH)).getImage();
         setTitle("PLAYERS SCORES");
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        score = new JTextArea(); //2
-        score.setEditable(false); //3
         */
-
 
         this.setLayout(new BorderLayout());
         scorImage = new ImageIcon(getClass().getResource(BACKGROUND_IMAGE_PATH)).getImage();
@@ -61,31 +46,32 @@ private JTextArea score;
         final JButton back = new GenericButton(BACK, b -> view.setScene(Scenes.MENU.getScene()));
         pSouth.setOpaque(false);
         pSouth.add(back);
-        this.add(pSouth, BorderLayout.SOUTH); //
+        this.add(pSouth, BorderLayout.SOUTH);
 
         score = new JTextArea();
         score.setEditable(false);
 
-        JScrollPane pane = new JScrollPane(score); //4
-        add(pane, BorderLayout.CENTER);  //5
+        final JScrollPane pane = new JScrollPane(score);
+        add(pane, BorderLayout.CENTER);
 
-        //metodo per caricare il file (addFile(FILE_TXT_PATH)
         addFile(FILE_TXT_PATH);
 
     }
 
-    private void addFile(String FILE_TXT_PATH) {
+    private final void addFile(String FILE_TXT_PATH) {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_TXT_PATH))) {
             String line;
+            
             while((line = reader.readLine()) != null) {
                 score.append(line + "\n");
             }
-        }catch (IOException error) {
+        } catch (final IOException error) {
                 JOptionPane.showMessageDialog(this, "error file reader", "error", 
                 JOptionPane.ERROR_MESSAGE);
             }
     }
+
     @Override
     public MusicPlayer getMusic() {
         return new LoopingMusicPlayerImpl(TRACK_PATH);
@@ -95,5 +81,4 @@ private JTextArea score;
     protected Image getImage() {
         return scorImage;
     }
-        
 }
