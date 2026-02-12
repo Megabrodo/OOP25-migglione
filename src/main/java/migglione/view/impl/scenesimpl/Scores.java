4843e6d7bd84c5c30d22a91e0a81ccdb0d641c2d
anton abramov <anton.abramov@studio.unibo.it>
2026-02-12 01:34:41 +0100

    Scores add JTextArea

--- src/main/java/migglione/view/impl/scenesimpl/Scores.java
+++ src/main/java/migglione/view/impl/scenesimpl/Scores.java
@@ -8,7 +8,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 
 import migglione.view.api.music.MusicPlayer;
 import migglione.view.api.music.MusicProvider;
@@ -16,27 +18,45 @@ import migglione.view.api.scenes.Scenes;
 import migglione.view.impl.SwingViewImpl;
 import migglione.view.impl.musicimpl.LoopingMusicPlayerImpl;
 
-public final class Scores extends AbstractGamePanel implements MusicProvider {
+public final class Scores extends JFrame {
 
 private static final String TRACK_PATH = "/soundtracks/ENA Dream BBQ.wav";
 private static final String BACKGROUND_IMAGE_PATH = "/images/utilities/title.png";
 private final transient Image scorImage;
 private static final String BACK = "Back";
+private static final String FILE_TXT_PATH = "";
+private JTextArea score;
 
-    public Scores(final SwingViewImpl view) {
+    public Scores(String FILE_TXT_PATH) { //final SwingViewImpl view
+        /*
         this.setLayout(new BorderLayout());
         scorImage = new ImageIcon(getClass().getResource(BACKGROUND_IMAGE_PATH)).getImage();
 
-        final JPanel pSouth = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        final JPanel pSouth = new JPanel(new FlowLayout(FlowLayout.CENTER));
         final JButton back = new GenericButton(BACK, b -> view.setScene(Scenes.MENU.getScene()));
 
         pSouth.setOpaque(false);
         pSouth.add(back);
-        this.add(pSouth, BorderLayout.SOUTH);
+        this.add(pSouth, BorderLayout.SOUTH);  */
 
-        
+        setTitle("PLAYERS SCORES");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        score = new JTextArea();
+        score.setEditable(false);
+
+        JScrollPane pane = new JScrollPane(score);
+        add(pane, BorderLayout.CENTER);
+
+        //metodo per caricare il file (addFile(FILE_TXT_PATH)
+
     }
 
+    private void addFile(FILE_TXT_PATH){
+
+    }
+    /* 
     @Override
     public MusicPlayer getMusic() {
         return new LoopingMusicPlayerImpl(TRACK_PATH);
@@ -46,4 +66,5 @@ private static final String BACK = "Back";
     protected Image getImage() {
         return scorImage;
     }
+        */
 }
