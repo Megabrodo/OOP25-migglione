65f575c171f379f3e1d01e5bb317647940e81a7b
anton abramov <anton.abramov@studio.unibo.it>
2026-02-14 20:29:11 +0100

    create class BorderedLine

---
+++ src/main/java/migglione/view/impl/scenesimpl/BorderedLine.java
@@ -0,0 +1,11 @@
+package migglione.view.impl.scenesimpl;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+
+public class BorderedLine extends JLabel{
+private Color borderedLine = Color.getHSBColor(63, 62, 90);
+
+
+}
