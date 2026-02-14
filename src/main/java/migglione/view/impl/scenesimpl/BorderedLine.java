ee30af04c1fffa8ead0012f98ff57a01c6e281b9
anton abramov <anton.abramov@studio.unibo.it>
2026-02-14 20:53:24 +0100

    BorderedLine colorPaint

--- src/main/java/migglione/view/impl/scenesimpl/BorderedLine.java
+++ src/main/java/migglione/view/impl/scenesimpl/BorderedLine.java
@@ -1,11 +1,22 @@
 package migglione.view.impl.scenesimpl;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JLabel;
 
 public class BorderedLine extends JLabel{
 private Color borderedLine = Color.getHSBColor(63, 62, 90);
+private float stroke = 6f;
 
+    public BorderedLine(final String FILE_TXT){
+        super(FILE_TXT);
+    }
 
+    void colorPaint(Graphics graphics) {
+
+        Graphics2D graphic = (Graphics2D) graphics;
+
+    }
 }
