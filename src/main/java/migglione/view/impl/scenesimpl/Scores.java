b80ca30838052b461ae4d3071e12fa389ad5cf03
anton abramov <anton.abramov@studio.unibo.it>
2026-02-15 02:53:22 +0100

    BorderedLine class and scrores try implementation

--- src/main/java/migglione/view/impl/scenesimpl/Scores.java
+++ src/main/java/migglione/view/impl/scenesimpl/Scores.java
@@ -33,13 +33,13 @@ private final transient Image scorImage;
 private static final String BACK = "Back";
 private static final String FILE_TXT_PATH = "/file/ScoreTable.txt";
 private JTextArea score;
+//private BorderedLine bordered = (BorderedLine) new JLabel();
 
 
     public Scores(final SwingViewImpl view) {
 
         this.setLayout(new BorderLayout());
         scorImage = new ImageIcon(getClass().getResource(BACKGROUND_IMAGE_PATH)).getImage();
-        this.score = new JTextArea();
 
         final JPanel pSouth = new JPanel(new FlowLayout(FlowLayout.CENTER));
         final JButton back = new GenericButton(BACK, b -> view.setScene(Scenes.MENU.getScene()));
@@ -47,31 +47,35 @@ private JTextArea score;
         pSouth.add(back);
         this.add(pSouth, BorderLayout.SOUTH);
 
-        //BorderedLine bord = new BorderedLine(FILE_TXT_PATH);
-        score = new JTextArea();
-        score.setEditable(false);
-        score.setOpaque(false);
-        score.setFont(new Font("Verdana", Font.PLAIN, 50));
-        score.setForeground(new Color(252, 64, 167));
-        
-        this.add(score, BorderLayout.CENTER); //
+        this.score = new JTextArea();
+        this.score.setEditable(false);
+        this.score.setOpaque(false);
+        this.score.setFont(new Font("Verdana", Font.PLAIN, 50));
+        this.score.setForeground(new Color(252, 64, 167));
 
         final JScrollPane pane = new JScrollPane(score);
         this.add(pane, BorderLayout.CENTER); 
         pane.setOpaque(false);
         pane.getViewport().setOpaque(false);
 
-        addFile(FILE_TXT_PATH);
-    }
+        readFile(FILE_TXT_PATH);
 
-    public void addFile(final String FILE_TXT_PATH) {
+        /*se togli i comenti per usare bordered non mi fa fare start */
+        //this.bordered = new BorderedLine(score.getText());
+
+        /* 
+        this.bordered.setEnabled(false);
+        this.bordered.setOpaque(false);
+        this.bordered.setFont(new Font("Verdana", Font.PLAIN, 50));
+        //*/
+    } 
+
+    public void readFile(final String FILE_TXT_PATH) {
 
         final Path path = Paths.get(System.getProperty("user.home"), ".migglione", "ScoreTable.txt");
 
         if (Files.exists(path)) {
             try (BufferedReader reader = Files.newBufferedReader(path)) {
-            //try (BufferedReader reader = new BufferedReader(new InputStreamReader(
-            //getClass().getResourceAsStream(FILE_TXT_PATH)))) {
 
                 String line;
 
@@ -89,7 +93,7 @@ private JTextArea score;
 
     public void refresh() {
         score.setText("");
-        addFile(FILE_TXT_PATH);
+        readFile(FILE_TXT_PATH);
     }
 
     @Override
